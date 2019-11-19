cade0fd1005cda65a8de6754896d1ebc45d349a0
Jack Fennell <C00220386@itcarlow.ie>
2019-11-19 16:42:49 +0000

    Feature 1

---
+++ Conveyor Belt/Assets/MoveObjectOnBelt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjectOnBelt : MonoBehaviour
+{
+    public float speed;
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
